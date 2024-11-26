3a67fe58fabb46c655475ca2a32d05776bf16ddf
ALONUCLEAR <alonrazka58@gmail.com>
2024-11-26 20:52:10 +0200

    feat: Health Check

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace teachers_lounge_server.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : Controller
+    {
+        [HttpGet(Name="Health Check")]
+        public ActionResult<string> HealthCheck()
+        {
+            return Ok("Service is healthy");
+        }
+    }
+}
