39d964176c36e1f600fa3a2237cb7c1817486094
ALONUCLEAR <alonrazka58@gmail.com>
2025-03-08 18:07:04 +0200

    fix: govIds should be ids

--- Entities/Street.cs
+++ Entities/Street.cs
@@ -4,22 +4,22 @@ namespace teachers_lounge_server.Entities
 {
     public class Street : GovernmentData
     {
-        public int municipalityFk;
+        public int municipalityId;
         public Street() : base() { }
-        public Street(int id, string name, int fk, int municipalityFk) : base(id, name, fk)
+        public Street(int id, string name, int municipalityId) : base(id, name)
         {
-            this.municipalityFk = municipalityFk;
+            this.municipalityId = municipalityId;
         }
         public Street(GovernmentData toCopy): base(toCopy) { }
         public Street(Street toCopy) : base(toCopy)
         {
-            this.municipalityFk = toCopy.municipalityFk;
+            this.municipalityId = toCopy.municipalityId;
         }
 
         public override BsonDocument ToBsonDocument()
         {
             var fullDocument = base.ToBsonDocument();
-            fullDocument.Add("municipalityFk", municipalityFk);
+            fullDocument.Add("municipalityId", municipalityId);
 
             return fullDocument;
         }
@@ -27,7 +27,7 @@ namespace teachers_lounge_server.Entities
         public static new Street FromBsonDocument(BsonDocument document)
         {
             Street result = new Street(GovernmentData.FromBsonDocument(document));
-            result.municipalityFk = document.GetValueOrDefault<int>("municipalityFk");
+            result.municipalityId = document.GetValueOrDefault<int>("municipalityId");
 
             return result;
         }
