757afff69885df1878f2e56993b5ee08664a2177
ALONUCLEAR <alonrazka58@gmail.com>
2025-10-09 16:49:33 +0300

    fix: add cors support for hosted client

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(corsPolicyName, policy =>
     {
-        policy.WithOrigins("http://localhost:4200", "https://localhost:4200")
+        policy.WithOrigins("http://localhost:4200", "https://localhost:4200", "https://teachers-lounge.netlify.app")
         .AllowAnyHeader()
         .AllowAnyMethod();
     });
